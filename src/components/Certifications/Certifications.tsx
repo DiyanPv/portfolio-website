086bc3f0356daddc2a8913846deb14769e5d5694
DiyanPv <diyanpetrov61@gmail.com>
2023-01-31 14:35:29 +0200

    margin-bottom fixed

--- src/components/Certifications/Certifications.tsx
+++ src/components/Certifications/Certifications.tsx
@@ -1,7 +1,7 @@
 import { CertificationCards } from "../Cards";
 export const Certifications = () => {
   return (
-    <div className="h-fit flex flex-col mb-8">
+    <div className="h-fit flex flex-col sm:pb-16">
         <div className="certificationTransition"></div>
       <div className="bg-certifications">
         <CertificationCards />
