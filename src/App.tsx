b42674b99ac78cd1dabfb248f0ba6b0666b0a29b
DiyanPv <diyanpetrov61@gmail.com>
2023-03-26 15:48:09 +0300

    small tweaks, added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import "./App.css";
 import { ProjectCards } from "./components/Cards/MappedCards/Cards";
 import { Content } from "./components/Layout/OpeningPage/Content";
 import { Header } from "./components/Layout/Header/Header";
-import { TechnologiesComponent } from "./components/Technologies/Technologies";
 import { Certifications } from "./components/Certifications/Certifications";
-function App() {
+import { Routes, Route } from "react-router-dom";
+const Home = () => {
   return (
     <div>
       <Header />
       <Content />
-      <ProjectCards />
-      <Certifications/>
+      {/* <ProjectCards />
+      <Certifications /> */}
     </div>
+  );
+};
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
 
--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,5 +1,20 @@
-import React from "react";
-
+import { useLocation } from "react-router-dom";
+import { logo } from "../../../assets/extras";
 export const Header = () => {
-  return <div></div>;
+  const location = useLocation();
+
+  return (
+    <div className="flex pt-4 text-white items-center sticky top-0 ">
+      <div className="flex grow">
+
+      </div>
+      <nav className=" grow ">
+        <ul className="flex flex-row justify-around">
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Certificates</li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
